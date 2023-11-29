c45a304ff86a59b62c43d1506af1a81a1de87706
Fayzan Ali Akhtar <fayzan585@gmail.com>
2023-11-29 23:52:54 +0500

    routes added in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors'
+// Importing Routes 
+const bookRouter = require('./routes/bookRoute');
+const borrowedbooksRouter = require('./routes/borrowedbooksRoute');
+const feedbackRouter = require('./routes/feedBackRoute');   
+const managerRouter = require('./routes/managerRoute');
+const staffRouter = require('./routes/staffRoute');
+const studentRouter = require('./routes/studentRoute');
 
 dotenv.config();
 
@@ -11,8 +18,23 @@ app.use(express.json())
 
 app.use(cors())
 
-//Import Routes
+// For Book Routes
+app.use('/api/book', bookRouter);
 
+// For BorrowedBooks Routes
+app.use('/api/borrowedbooks', borrowedbooksRouter);
+
+// For Feedback Routes
+app.use('/api/feedback', feedbackRouter);
+
+// For Manager Routes
+app.use('/api/manager', managerRouter);
+
+// For Staff Routes
+app.use('/api/staff', staffRouter);
+
+// For Student Routes
+app.use('/api/student', studentRouter);
 
 mongoose.connect(process.env.MONG_URI)
 .then(()=>{
