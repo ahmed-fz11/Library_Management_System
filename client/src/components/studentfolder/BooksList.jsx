74c8912d2e1471398786368a268845b3b67f2335
Fayzan Ali Akhtar <fayzan585@gmail.com>
2023-12-01 03:44:42 +0500

    Search added to book!

--- client/src/components/studentfolder/BooksList.jsx
+++ client/src/components/studentfolder/BooksList.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 import { book_all_URL, book_update_URL, borrowedbooks_add_URL } from '../../constant';
 
 const BooksList = ({ userInfo }) => {
   const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
   const fetchBooks = async () => {
     try {
       const response = await axios.post(book_all_URL); // Assuming GET request for fetching all books
@@ -54,13 +57,28 @@ const BooksList = ({ userInfo }) => {
     }
   };
   
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredBooks = searchTerm
+    ? books.filter(book =>
+        book.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : books;
 
   return (
     <div className="container mt-4">
       <h2>All Books in the Library</h2>
+      <FormControl
+        type="text"
+        placeholder="Search by book title"
+        className="mb-4"
+        onChange={handleSearch}
+      />
       <div className="row">
-        {books.map((book) => (
-          <div key={book._id} className="col-md-4 mb-4">
+          {filteredBooks.map((book) => (
+            <div key={book._id} className="col-md-4 mb-4">
             <Card>
               <Card.Body>
                 <Card.Title>{book.name}</Card.Title>
@@ -73,8 +91,9 @@ const BooksList = ({ userInfo }) => {
             </Card>
           </div>
         ))}
+        {filteredBooks.length === 0 && <p>No books found.</p>}
+        </div>
       </div>
-    </div>
   );
 };
 
